3618ba05637165bc152eca8046a1873f73e2428d
utsav132 <155137203+utsav132@users.noreply.github.com>
2024-03-01 16:03:24 +0530

    Update EMP.py
    database added

--- EMP.py
+++ EMP.py
@@ -1,42 +1,82 @@
-# Employee class
-class Employee:
+from OFFICE import Office 
+import pymongo
+
+#database
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["mnc_db"]
+country = db['country']
+
+# Country class
+class Country:
     # Class variable
-    emp_counter = 0
+    country_counter = 0
 
     # Initialize class with default values
-    def __init__(self, name, salary):
-        self.employee_id = Employee.generate_employee_id()
-        self.employee_name = name
-        self.employee_salary = salary
+    def __init__(self, name, iso_code, currency_code):
+        self.country_id = Country.generate_country_id()
+        self.country_name = name
+        self.country_iso_code = iso_code
+        self.country_currency_code = currency_code
+        self.office_list = []
+        self.data = {"_id":self.country_id,
+                     "name":self.country_name,
+                     "iso_code":self.country_iso_code,
+                     "currency_code":self.country_currency_code} 
+        
+        country.insert_one(self.data)
 
-    # Generate unique employee ID
+    # Generate unique country ID
     @classmethod
-    def generate_employee_id(cls):
-        Employee.emp_counter += 1
-        return Employee.emp_counter
+    def generate_country_id(cls):
+        Country.country_counter += 1
+        return Country.country_counter
 
-    # Getter for employee_id
-    def get_employee_id(self):
-        return self.employee_id
+    # Getter for country_id
+    def get_country_id(self):
+        return self.country_id
 
-    # Getter for employee_name
-    def get_employee_name(self):
-        return self.employee_name
+    # Getter for country_name
+    def get_country_name(self):
+        return self.country_name
 
-    # Getter for employee_salary, returns formatted string
-    def get_employee_salary(self):
-        return "${:,.2f}".format(self.employee_salary)
+    # Getter for country_iso_code
+    def get_country_iso_code(self):
+        return self.country_iso_code
+
+    # Getter for country_currency_code
+    def get_country_currency_code(self):
+        return self.country_currency_code
+    
+    # Setter for country_currency_code
+    def add_office(self, off):
+        self.office_list.append(off)
+        print(off.office_name,'office added at',self.country_name)
+
+    def show_office(self):
+        if not self.office_list:
+            print('no Office')
+            return
+        for off in self.office_list:
+            print(off.office_name)
+
+    def get_offices(self):
+        return None if not self.office_list  else self.office_list
 
 
 # Main function
 def main():
-    # Create an employee
-    employee = Employee("Caelius Dathan", 4900.0)
+    # Create a country
+    country = Country("United States", "US", "USD")
 
-    # Print the employee details
-    print(f"Employee ID: {employee.get_employee_id()}")
-    print(f"Name: {employee.get_employee_name()}")
-    print(f"Salary: {employee.get_employee_salary()}")
+    # Print the country details
+    print(f"Country ID: {country.get_country_id()}")
+    print(f"Name: {country.get_country_name()}")
+    print(f"ISO Code: {country.get_country_iso_code()}")
+    print(f"Currency Code: {country.get_country_currency_code()}")
+
+    office = Office("Head Office", "1234 Main St.", "Chicago", "IL", "60601")
+    country.add_office(office)
+    country.show_office()
 
 
 if __name__ == "__main__":
